bdef4f59702d323a89e3544d971366389e29d49e
zubeen <zubeenqadry@gmail.com>
2020-10-14 07:05:59 +0530

    added networked image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://img.freepik.com/free-photo/empty-wooden-table-with-smoke-float-up-dark-background_68495-135.jpg?size=626&ext=jpg'),
+          ),
+        ),
       ),
     ),
   );

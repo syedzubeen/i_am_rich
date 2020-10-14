2db12cc77acb6579c2c52674044c922080e45c6c
zubeen <zubeenqadry@gmail.com>
2020-10-14 08:56:06 +0530

    project complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
